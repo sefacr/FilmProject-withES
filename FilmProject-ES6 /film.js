77dbbd387ba554c986ece6982b768d0a2800c644
sefacr <aacarsefa@gmail.com>
2023-02-24 15:53:22 +0300

    ES6-example

---
+++ FilmProject-ES6 /film.js
@@ -0,0 +1,10 @@
+// //Film Constructor
+// function Film(title, director, url) {
+//   this.title = title;
+//   this.director = director;
+//   this.url = url;
+// }
+
+class Film {
+  constructor;
+}
